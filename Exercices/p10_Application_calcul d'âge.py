d41dbb0f936fb9ba8a580667c82bc8d018814108
SAURET Cyril <sauretcyril@outlook.fr>
2024-09-13 09:45:03 +0200

    Mise en forme exercices P10 et P11

--- Exercices/p10_Application_calcul d'âge.py
+++ Exercices/p10_Application_calcul d'âge.py
@@ -24,22 +24,22 @@ def Ctrl_Format_Date( SToDate):
     format = one_date.split("/")
     print(f"nbSep {nbSep} / len = {len(format)} -{format} -")
     #peut on aller à la ligne sur une condition ?
-    if nbSep==2 and len(format)==3 and (int(format[1])<=12 and int(format[1]!=0)) :
+    if nbSep == 2 and len(format) == 3 and (int(format[1]) <= 12 and int(format[1] ! =0 )) :
         yy = format[2]
         mm = format[1]
         dd = format[0]
         #print(f"le {dd}/{mm}/{yy}" )
-        SToDate['format_ok']=True
-        SToDate['date']=date(int(yy), int(mm), int(dd))
+        SToDate['format_ok'] = True
+        SToDate['date'] = date(int(yy), int(mm), int(dd))
     return SToDate
 
 
-SToDate={'format_ok':False}
-isok= SToDate["format_ok"]
+SToDate = {'format_ok':False}
+isok = SToDate["format_ok"]
 while not isok:
-    SToDate['date']=input("Quel est votre date de naissance ('JJ/MM/YYYY') ?")
-    SToDate['format_ok']=Ctrl_Format_Date(SToDate)
-    isok= SToDate["format_ok"]
+    SToDate['date'] = input("Quel est votre date de naissance ('JJ/MM/YYYY') ?")
+    SToDate['format_ok']= Ctrl_Format_Date(SToDate)
+    isok = SToDate["format_ok"]
 
 print(f"Vous êtes né(e) le {SToDate['date']}")
 calculer_age(SToDate['date'])
--- Exercices/p11_fonctions cap et iscap.py
+++ Exercices/p11_fonctions cap et iscap.py
@@ -5,35 +5,36 @@ def titlecase(s):
     return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
                   lambda mo: mo.group(0).capitalize(),
                   s)
-
-
+#print(titlecase(' chaine.   test, Ok,le Rire permet de viellire moins vite. le petit poucet, EtaiT Petit'))
+#----------------------------------------------------------------
+# majuscule sur la premiere lettre même si il y a des espaces avant
 def capitalize(chaine):
     
-    e1_index_first_letter=len(chaine) - len(chaine.lstrip())
-    e2_chaine=chaine[e1_index_first_letter:len(chaine)]
-    e3_space=chaine[0:e1_index_first_letter]
-    e4_chaine=e3_space+e2_chaine.capitalize()
+    e1_index_first_letter = len(chaine) - len(chaine.lstrip())
+    e2_chaine = chaine[e1_index_first_letter:len(chaine)]
+    e3_space = chaine[0:e1_index_first_letter]
+    e4_chaine = e3_space+e2_chaine.capitalize()
     return e4_chaine
+#capitalize(' chaine.   test, Ok') 
+#----------------------------------------------------------------
 
-#print (titlecase(ab))
 
-# majuscule sur la premiere lettre même si il y a des espaces avant
-#cap1(' chaine.   test, Ok') 
 
-def cap2(chaine,i):
+#----------------------------------------------------------------
+# Mettre en majuscule premiere lettre et lettre qui suit un . sans tenir compte des espace, mettre en minuscule 
+# les lettres qui doivent pas être en majuscule
+def cap(chaine,i):
+    #on met tout en minuscule sauf la premiere lettre 
     if i==0:
         chaine=capitalize(chaine)
     index_point = chaine.find('.', i)
-    #print (index_point)
-    print(f"{i}={chaine}")
-     
-    #print(len(chaine))
+    
     if (index_point != -1) and (index_point < len(chaine)):
         start_chaine=chaine[0:index_point+1 ]
         end_chaine=chaine[index_point+1:len(chaine)]
-        new_chaineM = cap1(end_chaine)
+        new_chaineM = capitalize(end_chaine)
         chaine=start_chaine+new_chaineM
-        chaine=cap2(chaine,index_point+1)
+        chaine=cap(chaine,index_point+1)
         
         newIndex = chaine.find('.', index_point) 
         if (newIndex != -1) and (newIndex < len(chaine)):
@@ -44,6 +45,4 @@ def cap2(chaine,i):
           
         
 
-print (cap2(' chaine.   test, Ok,le Rire permet de viellire moins vite. le petit poucet, EtaiT Petit',0))
-
-#print (cap1(' chaine.   test, Ok'))
+print (cap(' chaine.   test, Ok,le Rire permet de vieillir moins vite. le petit poucet, EtaiT Petit',0))
